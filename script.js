539bd2d8d5d514e01edb1ba9b99eeff99ee8fb95
alex <132571895+belcouact@users.noreply.github.com>
2025-03-22 16:46:48 +0800

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function callWorker() {
     try {
-        const response = await fetch("https://ark-ds.workers.dev/", {
+        const response = await fetch("https://ark-ds.workers.dev", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
