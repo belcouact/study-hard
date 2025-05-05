9ee094210cdba4dabefd449e1854304f97aa5da9
alex <132571895+belcouact@users.noreply.github.com>
2025-05-05 22:22:08 +0800

    1

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-// Configuration file for environment variables
+// Configuration file for environment variables in a JavaScript application
 window._env = {
-    ds_url: "https://ark-ds.5525899.workers.dev",
-    ds_key: "sk-db4f7c2e3c99454da5a799b92a0c9f9c"
+    ds_url: "https://chat-api.study-llm.me",
+    ds_key: "sk-db4f7c2e3c99454da5a799b92a0c9f9c",
+    // ds_url: "http://localhost:8000",
+    test_param: "test",
 }; 
